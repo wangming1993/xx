2fca8b2cde55ec56d15f0b5e95716921630fce26
wangming1993 <wangming19932008@163.com>
2018-08-26 14:37:55 +0800

    feat: use spring, mybatis

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spring + Mybatis Learning
+
+---
+
+## 开发工具
+
+- Eclipse
+
+## 开发环境
+
+- JDK `1.8.0_91`
+- Tomcat `apache-tomcat-8.0.53`
+- Maven `apache-maven-3.5.4`
+- MySQL `8.0.12`
+
---
+++ src/test/java/com/mike/xx/dao/AppointmentDaoTest.java
@@ -0,0 +1,31 @@
+package com.mike.xx.dao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mike.xx.BaseTest;
+import com.mike.xx.entity.Appointment;
+
+public class AppointmentDaoTest extends BaseTest {
+
+    @Autowired
+    private AppointmentDao appointmentDao;
+
+    @Test
+    public void testInsertAppointment() throws Exception {
+        long bookId = 1000;
+        long studentId = 12345678910L;
+        int insert = appointmentDao.insertAppointment(bookId, studentId);
+        System.out.println("insert=" + insert);
+    }
+
+    @Test
+    public void testQueryByKeyWithBook() throws Exception {
+        long bookId = 1000;
+        long studentId = 12345678910L;
+        Appointment appointment = appointmentDao.queryByKeyWithBook(bookId, studentId);
+        System.out.println(appointment);
+        System.out.println(appointment.getBook());
+    }
+
+}
